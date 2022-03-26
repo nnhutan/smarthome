c906bb6021fe8031082f28177d348cd09d126222
nnhutan <nhutan2001@gmail.com>
2022-03-26 15:13:06 +0700

    add login page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,22 +1,34 @@
 import { useContext } from "react";
 import AuthContext from "../../context/AuthContext";
 import { useNavigate } from "react-router-dom";
+import loginImg from "./login.png";
+import "./login.scss";
 
 const Login = () => {
   const { setIsLoggedIn } = useContext(AuthContext);
   const navigate = useNavigate();
   return (
-    <>
-      <h1>Login</h1>
-      <button
-        onClick={() => {
-          setIsLoggedIn(true);
-          navigate("/", { replace: true });
-        }}
-      >
-        Login
-      </button>
-    </>
+    <div className="login">
+      <div className="container">
+        <div className="content">
+          <h1 className="title">Login</h1>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              setIsLoggedIn(true);
+              navigate("/", { replace: true });
+            }}
+          >
+            <input type="text" placeholder="Username" required />
+            <input type="password" placeholder="Password" required />
+            <button type="submit">Login</button>
+          </form>
+        </div>
+        <div className="img">
+          <img src={loginImg} alt="login" />
+        </div>
+      </div>
+    </div>
   );
 };
 
