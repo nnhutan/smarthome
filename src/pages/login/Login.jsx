d13f62bd4c2f3c0c8ccc2f899fe13eb42255447f
nnhutan <nhutan2001@gmail.com>
2022-03-26 15:17:46 +0700

    update login page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import AuthContext from "../../context/AuthContext";
 import { useNavigate } from "react-router-dom";
 import loginImg from "./login.png";
@@ -6,21 +6,41 @@ import "./login.scss";
 
 const Login = () => {
   const { setIsLoggedIn } = useContext(AuthContext);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    // call api
+    setIsLoggedIn(true);
+    navigate("/", { replace: true });
+  };
+
   const navigate = useNavigate();
   return (
     <div className="login">
       <div className="container">
         <div className="content">
           <h1 className="title">Login</h1>
-          <form
-            onSubmit={(e) => {
-              e.preventDefault();
-              setIsLoggedIn(true);
-              navigate("/", { replace: true });
-            }}
-          >
-            <input type="text" placeholder="Username" required />
-            <input type="password" placeholder="Password" required />
+          <form onSubmit={handleLogin}>
+            <input
+              type="text"
+              placeholder="Username"
+              required
+              value={username}
+              onChange={(e) => {
+                setUsername(e.target.value);
+              }}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              required
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
             <button type="submit">Login</button>
           </form>
         </div>
